050c3a72842d1ba3b1cc77cbf65af13f5d68df08
Yuta Tamaru <y-tamaru@alterbooth.com>
2023-02-15 16:52:34 +0900

    [modify] create selectebox in hello/index.cshtml

--- SampleMVCApp/Controllers/HelloController.cs
+++ SampleMVCApp/Controllers/HelloController.cs
@@ -4,24 +4,32 @@ namespace SampleMVCApp.Controllers
 {
     public class HelloController : Controller
     {
+        public List<string> list;
+
+        public HelloController()
+        {
+            list = new List<string>();
+            list.Add("Japan");
+            list.Add("USA");
+            list.Add("India");
+            list.Add("UK");
+        }
         public IActionResult Index()
         {
-            ViewData["message"] = "Input your data:";
-            ViewData["name"] = "";
-            ViewData["mail"] = "";
-            ViewData["tel"] = "";
+            ViewData["message"] = "Select item:";
+            ViewData["list"] = "";
+            ViewData["listdata"] = list;
             return View();
         }
 
         [HttpPost]
         public IActionResult Form()
         {
-            ViewData["name"] = Request.Form["name"];
-            ViewData["mail"] = Request.Form["mail"];
-            ViewData["tel"] = Request.Form["tel"];
-            ViewData["message"] = ViewData["name"] + ", " +
-                    ViewData["mail"] + ",  " + ViewData["tel"];
+            ViewData["message"] = '"' + Request.Form["list"] + '"' + " selected.";
+            ViewData["list"] = Request.Form["list"];
+            ViewData["listdata"] = list;
             return View("Index");
         }
     }
 }
+
