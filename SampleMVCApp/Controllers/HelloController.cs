17b0a488ce9ba29e4409dd989758970da8fc7850
Yuta Tamaru <y-tamaru@alterbooth.com>
2023-02-15 22:59:58 +0900

    [modify] object saved in the session

--- SampleMVCApp/Controllers/HelloController.cs
+++ SampleMVCApp/Controllers/HelloController.cs
@@ -1,27 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
 
-namespace SampleMVCApp.Controllers
+namespace SampleMVCApp.Controllers;
+
+public class HelloController : Controller
 {
-    public class HelloController : Controller
-    {
-        [HttpGet("Hello/{id?}/{name?}")]
-        public IActionResult Index(int id, string name)
-        {
-            ViewData["message"] = "※セッションにIDとNameを保存しました。";
-            HttpContext.Session.SetInt32("id", id);
-            HttpContext.Session.SetString("name", name);
-            return View();
-        }
 
-        [HttpGet("Other")]
-        public IActionResult Other()
+    [HttpGet("Hello/{id?}/{name?}")]
+    public IActionResult Index(int id, string name)
+    {
+        ViewData["message"] = "※セッションにIDとNameを保存しました。";
+        MyData ob = new MyData(id, name);
+        String s = ObjectToString(ob);
+        HttpContext.Session.SetString("object", s);
+        ViewData["object"] = ob;
+        return View();
+    }
+
+    [HttpGet("Other")]
+    public IActionResult Other()
+    {
+        ViewData["message"] = "保存されたセッションの値を表示します。";
+        String s = HttpContext.Session.GetString("object") ?? "";
+        ViewData["object"] = StringToObject(s);
+        return View("Index");
+    }
+
+    // convert object to String.
+    private String ObjectToString(MyData ob)
+    {
+        return JsonSerializer.Serialize<MyData>(ob);
+    }
+
+    // convert String to object.
+    private MyData? StringToObject(String s)
+    {
+        MyData? ob;
+        try
         {
-            ViewData["id"] = HttpContext.Session.GetInt32("id");
-            ViewData["name"] = HttpContext.Session.GetString("name");
-            ViewData["message"] = "保存されたセッションの値を表示します。";
-            return View("Index");
+            ob = JsonSerializer.Deserialize<MyData>(s);
         }
+        catch (Exception e)
+        {
+            ob = new MyData(0, "noname");
+        }
+        return ob;
     }
 }
 
+[Serializable]
+class MyData
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
 
+    public MyData(int id, string name)
+    {
+        this.Id = id;
+        this.Name = name;
+    }
+
+    override public string ToString()
+    {
+        return "<" + Id + ": " + Name + ">";
+    }
+}
