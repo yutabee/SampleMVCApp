d5d84f1db370491c4503b91063760ccef954dcfb
Yuta Tamaru <y-tamaru@alterbooth.com>
2023-02-15 09:05:54 +0900

    [add] create hellocontroller

---
+++ SampleMVCApp/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleMVCApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
